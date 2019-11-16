02c322fa0e079a4fb393fbda41dd9bd957a32e20
Vivian Imbriotis <vivian.imbriotis@gmail.com>
2019-11-17 10:35:26 +1100

    Cmap to repr arg(z)

--- modplot.py
+++ modplot.py
@@ -1,5 +1,6 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
 from matplotlib import cm
 import numpy as np
 
@@ -33,11 +34,22 @@ def modular_plot_func(func, bounds="circle", func_str="f(z)", **kwargs):
     ax.set_xlabel("Re[Z]")
     ax.set_ylabel("Im[Z]")
     ax.set_zlabel("|%s|"%func_str)
+    args = np.angle(Z)
+    norm = Normalize(vmin = -np.pi, vmax=np.pi)
     Z = np.abs(Z)
+    print(np.max(args))
+    print(np.min(args))
     if "max_z" in kwargs:
         Z = np.minimum(Z,kwargs["max_z"]*np.ones(Z.shape))
-    surf = ax.plot_surface(X,Y,Z, cmap = cm.coolwarm)
+        
+    surf = ax.plot_surface(X,Y,Z,
+                           cmap=cm.hsv, facecolors = cm.hsv(norm(args)),
+                           rcount=100,ccount=100, shade=True)
+    cb = fig.colorbar(surf, extend = "both",ticks=[0,0.5,1], aspect=30,
+                      shrink=0.8)
+    cb.set_label("Arg(f(z))")
+    cb.ax.set_yticklabels(["-π", "0", "π"])
     plt.show()
 
-modular_plot_func(lambda z:1/(1+z**2),bounds="square",ranges=[-2,2,-2,2],
-                  func_str="1/(1+Z**2)", max_z = 3, res = 1000)
+modular_plot_func(lambda z:np.exp(-(z*z)),bounds="square",ranges=[-2,2,-2,2],
+                  func_str="exp(z)", max_z = 3, res = 1000)
